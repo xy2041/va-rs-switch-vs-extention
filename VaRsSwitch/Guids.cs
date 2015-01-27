c0273db8f438c7936b4bb1c5a0a17b3787cd8370
keht <xy2041@gmail.com>
2015-01-27 23:16:36 +0300

    basic project setup

---
+++ VaRsSwitch/Guids.cs
@@ -0,0 +1,14 @@
+﻿// Guids.cs
+// MUST match guids.h
+using System;
+
+namespace NBSCompany.VaRsSwitch
+{
+    static class GuidList
+    {
+        public const string guidVaRsSwitchPkgString = "fedce980-2431-480b-b205-c16d67cb95ac";
+        public const string guidVaRsSwitchCmdSetString = "45706c85-de92-417b-a9f8-0d41ed0a6cd3";
+
+        public static readonly Guid guidVaRsSwitchCmdSet = new Guid(guidVaRsSwitchCmdSetString);
+    };
+}
---
+++ VaRsSwitch/PkgCmdID.cs
@@ -0,0 +1,13 @@
+﻿// PkgCmdID.cs
+// MUST match PkgCmdID.h
+using System;
+
+namespace NBSCompany.VaRsSwitch
+{
+    static class PkgCmdIDList
+    {
+        public const uint cmdidToggleResharperOrVisualAssist =        0x100;
+
+
+    };
+}
